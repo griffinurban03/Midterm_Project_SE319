e5ca47fac759215017e195db78a17c0155f08677
Griffin <148525371+griffinurban03@users.noreply.github.com>
2024-12-11 16:37:56 -0600

    adding final project folder

---
+++ FINAL_PROJECT_2s2/frontend/src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+
+return ();
+
+}
+
+export default App;
